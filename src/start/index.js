97aa7ec8bcad51d25c32513b655bac80f37a185f
Apoorv Saini <apoorvsaini9@gmail.com>
2018-03-13 00:51:13 -0400

    Initial commit

---
+++ src/start/index.js
@@ -0,0 +1,12 @@
+export const greet = () => {
+    return "Hello World!";
+  };
+  
+  export const trial = () => {
+    return "<div>PIPE</div>";
+  };
+  
+  export const bye = () => {
+    return "See ya!";
+  };
+  
